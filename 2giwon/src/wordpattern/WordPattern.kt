60eeb65188f53e526171578dea794d84d1dad1f4
2giwon <2giwon@gmail.com>
2020-09-18 10:07:14 +0900

    #01 속도 개선
    - HashMap이 mutableMapOf보다 빠름

--- 2giwon/src/wordpattern/WordPattern.kt
+++ 2giwon/src/wordpattern/WordPattern.kt
@@ -3,13 +3,12 @@ package wordpattern
 class Solution {
     fun wordPattern(pattern: String, s: String): Boolean {
         val sList = s.split(" ")
-        val map = mutableMapOf<Char, String>()
+        val map = HashMap<Char, String>()
 
         if (sList.size != pattern.length) return false
         for (i in sList.indices) {
             if (map[pattern[i]] == null) {
-                if (map.values.find { it == sList[i] }?.length ?: 0 > 1) return false
-
+                if (map.containsValue(sList[i])) return false
                 map[pattern[i]] = sList[i]
             } else {
                 if (map[pattern[i]] != sList[i]) {
