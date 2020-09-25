69e64dde6bb83b8c0d8e4d79b60c6dffbc0b130d
2giwon <2giwon@gmail.com>
2020-09-25 13:49:53 +0900

    #02 https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
    풀이

---
+++ 2giwon/src/timecomplexity/TapeEquilibrium.kt
@@ -0,0 +1,21 @@
+package timecomplexity
+
+import kotlin.math.abs
+import kotlin.math.min
+
+class TapeEquilibrium {
+    fun tapeEquilibrium(tape: IntArray): Int {
+        var front = 0
+        val total = tape.sum()
+        var result = Int.MAX_VALUE
+
+        for (p in 0 until tape.lastIndex) {
+            front += tape[p]
+            val end = total - front
+            result = min(result, abs(end - front))
+        }
+
+
+        return result
+    }
+}
