e183f6c4abd58318328e9595e2530bcfa1c0148e
Hyeonseok Kang <kekemusa37@gmail.com>
2023-04-18 12:13:37 +0900

    Update Merge Two Sorted Lists.kt

--- BeokBeok/Merge Two Sorted Lists.kt
+++ BeokBeok/Merge Two Sorted Lists.kt
@@ -8,47 +8,17 @@
  * }
  */
 class Solution {
-    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
-        if (l1 == null && l2 == null) return null
-        if (l1 == null) return l2
-        if (l2 == null) return l1
-
-        val result = ListNode(0)
-        if (l1.`val` > l2.`val`) {
-            result.`val` = l2.`val`
-            result.next = ListNode(0)
-            merge(l1, l2.next, result.next)
-        } else {
-            result.`val` = l1.`val`
-            result.next = ListNode(0)
-            merge(l1.next, l2, result.next)
-        }
-        return result
-    }
-    
-    fun merge(l1: ListNode?, l2: ListNode?, result: ListNode?) {
-        if (l1 == null && l2 == null) return
-
-        when {
-            l1 == null && l2 != null -> {
-                result?.`val` = l2.`val`
-                if (l2.next != null) result?.next = ListNode(0)
-                merge(l1, l2.next, result?.next)
-            }
-            l1 != null && l2 == null -> {
-                result?.`val` = l1.`val`
-                if (l1.next != null) result?.next = ListNode(0)
-                merge(l1.next, l2, result?.next)
-            }
-            l1?.`val` ?: 0 > l2?.`val` ?: 0 -> {
-                result?.`val` = l2?.`val` ?: 0
-                result?.next = ListNode(0)
-                merge(l1, l2?.next, result?.next)
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        return when {
+            list1 == null -> list2
+            list2 == null -> list1
+            list1.`val` <= list2.`val` -> {
+                list1.next = mergeTwoLists(list1.next, list2)
+                list1
             }
             else -> {
-                result?.`val` = l1?.`val` ?: 0
-                result?.next = ListNode(0)
-                merge(l1?.next, l2, result?.next)
+                list2.next = mergeTwoLists(list1, list2.next)
+                list2
             }
         }
     }
