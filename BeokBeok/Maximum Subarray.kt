0bcff1c1eb6d57795d0931162da35f41528e1ae0
Hyeonseok Kang <kekemusa37@gmail.com>
2023-05-13 22:26:33 +0900

    덧셈 결괏값을 저장하지 않도록 개선

--- BeokBeok/Maximum Subarray.kt
+++ BeokBeok/Maximum Subarray.kt
@@ -1,12 +1,10 @@
 class Solution {
     fun maxSubArray(nums: IntArray): Int {
         var result = nums[0]
-        val sumList = mutableListOf<Int>().apply {
-            add(nums[0])
-        }   
+        var max = nums[0]
         for (i in 1 until nums.size) {
-            sumList.add(nums[i] + if (sumList[i - 1] > 0) sumList[i - 1] else 0)
-            result = result.coerceAtLeast(sumList.last())
+            max = nums[i] + if (max > 0) max else 0
+            result = result.coerceAtLeast(max)
         }
         return result
     }
